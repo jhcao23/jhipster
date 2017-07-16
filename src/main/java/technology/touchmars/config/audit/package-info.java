3d8bb356381915bb990b5b371f7798b204d7416c
John Cao <jhcao23@gmail.com>
2017-07-15 20:42:10 -0400

    update master to version 4.6.1

---
+++ src/main/java/technology/touchmars/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package technology.touchmars.config.audit;
---
+++ src/main/java/technology/touchmars/web/websocket/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * WebSocket services, using Spring Websocket.
+ */
+package technology.touchmars.web.websocket;
